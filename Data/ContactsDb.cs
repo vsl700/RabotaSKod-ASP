6dd4b61c3cc2e4f3aa869611c0768c449d038040
vsl700 <51147745+vsl700@users.noreply.github.com>
2021-11-02 16:46:26 +0200

    Views screwed up
    (searches for Index in Contacts, I dunno why, maybe we have to move all the views to one folder)

--- Data/ContactsDb.cs
+++ Data/ContactsDb.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RabotaSKod_ASP.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace RabotaSKod_ASP.Data
 {
--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RabotaSKod_ASP.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +25,7 @@ namespace RabotaSKod_ASP
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ContactsDb>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
         }
 
@@ -39,8 +42,14 @@ namespace RabotaSKod_ASP
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore‐hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection(); app.UseStaticFiles();
-            app.UseRouting(); app.UseAuthorization(); app.UseEndpoints(endpoints =>
+            app.UseHttpsRedirection(); 
+            app.UseStaticFiles();
+
+            app.UseRouting(); 
+            
+            app.UseAuthorization(); 
+            
+            app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
